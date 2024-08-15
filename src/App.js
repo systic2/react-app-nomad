89d305e407d61a5064f09ebd06f353621b235878
systic <llsysticll@gmail.com>
2024-08-15 22:49:37 +0900

    Cleanup

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
+function Hello() {
+  useEffect(() => {
+    console.log("hi :)");
+    return () => console.log("bye :(");
+  }, []);
+  return <h1>Hello</h1>;
+}
+
 function App() {
   const [counter, setValue] = useState(0);
   const [keyword, setKeyword] = useState("");
+  const [showing, setShowing] = useState(false);
   const onClick = () => setValue((prev) => prev + 1);
   const onChange = (event) => setKeyword(event.target.value);
+  const onClick2 = () => setShowing((prev) => !prev);
   useEffect(() => {
     console.log("I run only once.");
   }, []);
@@ -29,6 +39,9 @@ function App() {
       />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me!</button>
+      <hr />
+      {showing ? <Hello /> : null}
+      <button onClick={onClick2}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }

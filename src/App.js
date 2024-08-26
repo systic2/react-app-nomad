f9f24435ad82f70cb96c7d14397e170a8c0a571a
systic <llsysticll@gmail.com>
2024-08-26 21:31:09 +0900

    Movie App part One

--- src/App.js
+++ src/App.js
@@ -2,46 +2,37 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [selected, setSelected] = useState([]);
-  const onChange = (event) => {
-    setSelected(JSON.parse(event.target.value));
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
   };
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
-  console.log(selected.name);
+  console.log(movies);
   return (
     <div>
-      <h1>The Coins {loading ? "" : `(${coins.length})`}</h1>
       {loading ? (
-        <strong>Loading...</strong>
+        <h1>Loading...</h1>
       ) : (
-        <select onChange={onChange}>
-          {coins.map((coin, index) => (
-            <option key={index} id={coin.name} value={JSON.stringify(coin)}>
-              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-            </option>
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map(genre => <li key={genre}>{genre}</li>)}
+              </ul>
+            </div>
           ))}
-        </select>
-      )}
-      {selected === 0 ? (
-        ""
-      ) : (
-        <ul>
-          {coins.map((coin, index) => (
-            <li key={index}>
-              {coin.name} ({coin.symbol}) :{" "}
-              {coin.quotes.USD.price / selected.quotes.USD.price}{" "}
-              {selected.symbol}
-            </li>
-          ))}
-        </ul>
+        </div>
       )}
     </div>
   );

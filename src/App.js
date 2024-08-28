7aedf7c6b2d79e56d29d4014e9ebb80b98ed7bb6
systic <llsysticll@gmail.com>
2024-08-28 22:29:31 +0900

    React Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import { useState, useEffect } from "react";
 import Movie from "./components/Movie";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Routes,
+  Router,
+  BrowserRouter,
+} from "react-router-dom";
+import Home from "./routes/Home"
+import Detail from './routes/Detail';
 
 function App() {
-  return null;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/hello' element={<h1>Hello!</h1>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,7 @@
 function Detail() {
-  return <h1>Detail</h1>
+  return <div>
+    <h1>Detail</h1>
+  </div>;
 }
 
 export default Detail;
